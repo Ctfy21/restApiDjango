e46f208e919d7ae7b8c7726cd4b57c7586d0176c
Александр Фрейманс <Ctfy21>
2024-01-31 22:43:27 +0300

    create CurrentValues, Variety, Experiment models

--- pheno/restApiPheno/models.py
+++ pheno/restApiPheno/models.py
@@ -11,16 +11,24 @@ class Variety(models.Model):
     sequence_number = models.IntegerField(null=True)
     
     def __str__(self):
-        return self.title
+        return self.title 
     
-class Regime(models.Model):
-    title = models.CharField(max_length=200)
-    temperature = models.FloatField()
-    
+class CurrentValues(models.Model):
+    time_create = models.DateTimeField(auto_now_add = True)
+    time_update = models.DateTimeField(auto_now = True)
+    variety = models.ForeignKey('Variety', on_delete=models.PROTECT, null = False)
+    box = models.ForeignKey('Box', on_delete=models.PROTECT, null = False)
+    recurrence = models.PositiveSmallIntegerField(max_value=100)
+    regime = models.FloatField()
+    all_plants = models.PositiveSmallIntegerField(max_value=30)
+    live_plants = models.PositiveSmallIntegerField(max_value=30)
+    grown_plants_value = models.PositiveSmallIntegerField(min_value=0, max_value=9)
+    live_plants_percent = models.FloatField()
+
     def __str__(self):
-        return self.title   
-    
-class Recurrence(models.Model):
-    title
+        return self.time_create
+
+class Experiment(models.Model):
+     
 
     
